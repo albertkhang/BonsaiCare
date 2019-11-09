f46eb5a9b54387287d12192935c15fa61e27cb3d
albertkhang <albertkhang2017@gmail.com>
2019-11-09 18:12:57 +0700

    add schedule activities, add manage activities

---
+++ app/src/main/java/com/albertkhang/bonsaicare/activity/manage/ManageBonsaiActivity.java
@@ -0,0 +1,16 @@
+package com.albertkhang.bonsaicare.activity.manage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.albertkhang.bonsaicare.R;
+
+public class ManageBonsaiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_bonsai);
+    }
+}
---
+++ app/src/main/java/com/albertkhang/bonsaicare/activity/schedule/ScheduleItemActivity.java
@@ -0,0 +1,16 @@
+package com.albertkhang.bonsaicare.activity.schedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.albertkhang.bonsaicare.R;
+
+public class ScheduleItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_item);
+    }
+}
